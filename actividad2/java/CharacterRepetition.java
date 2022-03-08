8e58b7fad5cf095f618db056ba77172b6f181889
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-03-07 21:08:50 -0300

    Finalizacion del ejercicio en Java

--- actividad2/java/CharacterRepetition.java
+++ actividad2/java/CharacterRepetition.java
@@ -2,23 +2,8 @@ import java.util.*;
 
 public class CharacterRepetition {
     public static HashMap<Character, Integer> getUniqueLettersRepetition(String myString) {
-        //Creamos un Map para almacenar la sgnt informacion:
-        //  {
-        //      "caracter":numero de veces que se repite
-        //  }
-        //  Ejemplo:
-        //  {
-        //      'a':2,
-        //      'b':1          
-        //  }
-        //
-        //
         HashMap<Character, Integer> uniqueLettersMap = new HashMap<>();
 
-        //Recorremos la cadena ingresada 
-        //y verificamos si es que la letra ya existe en el map
-        //Si no existe lo agregamos e inicializamos la repeticion a 1
-        //Sino incrementamos el valor de la repeticion para esa letra
         for (int i = 0; i < myString.length(); i++) {
             char currentLetter = myString.charAt(i);
             if (!uniqueLettersMap.containsKey(currentLetter)) {
@@ -32,18 +17,22 @@ public class CharacterRepetition {
         return uniqueLettersMap;
     }
 
+    public static void printLettersRepetition(HashMap<Character, Integer> uniqueLettersMap) {
+        for (Map.Entry<Character, Integer> uniqueLetterWithRepetition : uniqueLettersMap.entrySet()) {
+            System.out.println("La Letra " + uniqueLetterWithRepetition.getKey() + " se repite "
+                    + uniqueLetterWithRepetition.getValue() + " veces.");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese una cadena: ");
         String myString = scanner.nextLine();
 
-        HashMap<Character, Integer> allUniqueLettersWithRepetition = CharacterRepetition
+        HashMap<Character, Integer> allUniqueLettersRepetition = CharacterRepetition
                 .getUniqueLettersRepetition(myString);
 
-        for (Map.Entry<Character, Integer> uniqueLetterWithRepetition : allUniqueLettersWithRepetition.entrySet()) {
-            System.out.println("La Letra " + uniqueLetterWithRepetition.getKey() + " se repite "
-                    + uniqueLetterWithRepetition.getValue() + " veces.");
-        }
+        CharacterRepetition.printLettersRepetition(allUniqueLettersRepetition);
 
         scanner.close();
 
